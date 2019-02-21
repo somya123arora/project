de59e8ee85662810545cc85a0eefbf88e6426bf9
Somya Arora <somya.arora@INsomyaarora.local>
2019-02-21 11:38:34 +0530

    SA: last commit

---
+++ src/test/java/com/testing/pageobjects/DashboardPage.java
@@ -0,0 +1,24 @@
+package com.testing.pageobjects;
+
+import com.testing.frameworkautojava.BasePage;
+import com.testing.frameworkautojava.ReusableFunctions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DashboardPage extends BasePage {
+
+
+    public DashboardPage(ReusableFunctions driver)
+    {    super(driver);
+        PageFactory.initElements(ReusableFunctions.getDriver(),this);
+
+    }
+
+
+    @FindBy(xpath = "//*[@placeholder='Email']")
+    WebElement Dashboardheadingicon;
+
+
+}
+
